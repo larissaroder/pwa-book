20ee396c315986ff2b77d51dc563aa8e73a6eab7
Larissa <larissaroder@gmail.com>
2018-09-10 23:39:42 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,5 @@
- <template>
-    <div id="app">
-      <header>
-      </header>
-      <main>
-        <div class="wrapper">
-          <div class="books">
-              <book v-for="list in lists" :key="list.imageUrl" :list="list"></book>
-          </div>
-        </div>
-      </main>
-    </div>
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
-
-<script>
-import data from './db.json';
-import Book from './components/Book';
-
-export default {
-  name: 'app',
-  components: {
-  'book': Book
-  },
-  data() {
-    return {
-      lists: []
-    }
-  },
-  created() {
-    this.lists = data;
-  }
-}
-</script>
-
- <style scoped lang="css">
-  .books {
-    display: flex;
-    flex-direction: column;
-    padding: 3px;
-  }
-  .book:hover {
-      transform: translateY(-0.5em);
-      background: #EBEBEB;
-  }
-  img {
-      display: block;
-      width: 100%;
-  }
-  @media only screen and (min-width: 900px) {
-    .books {
-      flex-direction: row;
-    }
-  }
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Book from '@/components/Book'
+import Books from '@/components/Books'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Book',
-      component: Book
+      name: 'Books',
+      component: Books
     }
   ]
 })
