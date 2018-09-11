0da09b6d27a5b010c488c2de837048358d0ed451
Larissa <larissaroder@gmail.com>
2018-09-10 23:16:54 -0300

    first commit

---
+++ src/components/Book.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="book">
+    <div>
+      <img :src="list.imageUrl" :alt="list.title">
+      <h4>{{ list.title }}</h4>
+      <p>{{ list.description }}</p>
+    </div>
+  </div>
+</template>
+
+ <script>
+    export default {
+    props: ['list'],
+    name: 'book'
+    }
+</script>
+
+ <style scoped lang="css">
+  .book {
+    background: #F5F5F5;
+    padding: 10px;
+    width: 100%;
+    cursor: pointer;
+    transition: all 100ms ease-in-out;
+  }
+  .book:hover {
+    transform: translateY(-0.5em);
+    background: #EBEBEB;
+  }
+  img {
+    display: block;
+    width: 100%;
+  }
+</style>
